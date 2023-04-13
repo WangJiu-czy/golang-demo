0b16ba47be49490a044cf6769ae6686e2c3bcf99
1412849797 <wangjiu2019@gmail.com>
2023-04-13 23:10:26 +0800

    day6

---
+++ 7-days/GeeRPC/day6-load-balance/client_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net"
+	"os"
+	"runtime"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestClient_dialTimeout(t *testing.T) {
+	t.Parallel()
+	l, _ := net.Listen("tcp", ":0")
+	f := func(conn net.Conn, opt *Option) (client *Client, err error) {
+		_ = conn.Close()
+		time.Sleep(time.Second * 2)
+		return nil, nil
+	}
+	t.Run("timeout", func(t *testing.T) {
+		_, err := dialTimeout(f, "tcp", l.Addr().String(), &Option{ConnectTimeout: time.Second})
+		_assert(err != nil && strings.Contains(err.Error(), "connect timeout"), "expect a timeout error")
+	})
+	t.Run("0", func(t *testing.T) {
+		_, err := dialTimeout(f, "tcp", l.Addr().String(), &Option{ConnectTimeout: 0})
+		_assert(err == nil, "0 means no limit")
+	})
+}
+
+func TestXDial(t *testing.T) {
+	if runtime.GOOS == "linux" {
+		ch := make(chan struct{})
+		addr := "/tmp/geerpc.sock"
+		go func() {
+			_ = os.Remove(addr)
+			l, err := net.Listen("unix", addr)
+			if err != nil {
+				t.Fatal("failed to listen unix socket")
+			}
+			ch <- struct{}{}
+			Accept(l)
+		}()
+		<-ch
+		_, err := XDial("unix@" + addr)
+		_assert(err == nil, "failed to connect unix socket")
+	}
+}
